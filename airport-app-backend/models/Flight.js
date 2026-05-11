3c6443fad57892f0ec51c825c1256da86bb60ba3
nayana <nayananayan50716>
2026-05-11 18:32:36 +0530

    Fixed module import paths

--- airport-app-backend/models/Flight.js
+++ airport-app-backend/models/Flight.js
@@ -1,4 +1,14 @@
 import mongoose from "mongoose";
+import Flight from "../models/Flight.js";
+
+const seatSchema = new mongoose.Schema({
+  number: String,
+  isBooked: {
+    type: Boolean,
+    default: false,
+  },
+  
+});
 
 const flightSchema = new mongoose.Schema({
   flightNumber: String,
@@ -7,7 +17,9 @@ const flightSchema = new mongoose.Schema({
   departureTime: String,
   arrivalTime: String,
   price: Number,
-  seatsAvailable: Number
+
+  // ✅ NEW SEAT SYSTEM
+  seats: [seatSchema],
 });
 
 export default mongoose.model("Flight", flightSchema);
